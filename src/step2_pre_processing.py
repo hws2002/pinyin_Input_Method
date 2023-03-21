9adf1c0b5f76a6bc56e3411440338ba1a9c88a9a
hws2002 <hanys21@mails.tsinghua.edu.cn>
2023-03-21 09:17:28 +0800

    第二步 : pre_processing 删除'原标题'

--- src/step2_pre_processing.py
+++ src/step2_pre_processing.py
@@ -19,13 +19,16 @@ chinese_pattern = re.compile('[\u4e00-\u9fff]+')
     
 for training_set in training_sets:
     filename = os.path.basename(training_set)
-    print(filename)
+    print(f"pre processing {filename}...")
     with open(f'../../语料库/sina_news_gbk/cleaned_data/{filename}','w',encoding='gbk') as f1:
         with open(training_set, 'r', encoding='gbk') as f2:
             for line in f2:
                 line = line.strip()
                 chinese_text = ''.join(chinese_pattern.findall(line))
-                f1.write(chinese_text)
+                if chinese_text[0:3] == '原标题':
+                    f1.write(chinese_text[3:])
+                else:
+                    f1.write(chinese_text)
                 f1.write('\n')
             f2.close()
         f1.close()
---
+++ src/step3_viterbi.py
@@ -0,0 +1,6 @@
+import json
+
+# bigram model
+with open('../data/frequency_dict.json', 'r', encoding='gbk') as f:
+    frequency_dict = json.load(f)
+
